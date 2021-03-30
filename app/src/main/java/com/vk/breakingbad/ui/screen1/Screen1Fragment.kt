9a6b4c9df946a3d6e3834ea4bc4572c8b1e44d79
vk <valeriia.koltsova@gmail.com>
2021-03-29 22:50:35 -0400

    added filters for search and spinner

--- app/src/main/java/com/vk/breakingbad/ui/screen1/Screen1Fragment.kt
+++ app/src/main/java/com/vk/breakingbad/ui/screen1/Screen1Fragment.kt
@@ -3,6 +3,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -13,11 +14,14 @@ import com.vk.breakingbad.R
 import com.vk.breakingbad.databinding.Screen1Binding
 import com.vk.breakingbad.datasource.Character
 import kotlinx.coroutines.launch
-class Screen1Fragment : Fragment() {
+import java.lang.IllegalStateException
 
+class Screen1Fragment : Fragment() {
     private lateinit var binding: Screen1Binding
 
     var characters: List<Character>? = null
+    var season: String = ""
+    var input: String? = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,42 +36,65 @@ class Screen1Fragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = Screen1Binding.bind(view)
-        val spinner = binding.spinner
-        val recyclerView = binding.recyclerView
-        recyclerView.layoutManager = GridLayoutManager(context, 2)
-        binding.searchEditText.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(query: String?): Boolean {
+        binding.recyclerView.layoutManager = GridLayoutManager(context, 2)
+
+        setListeners()
+        refreshCharacters()
+    }
+
+    private fun setListeners() {
+        binding.searchEditText.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean { //когда enter нажат после ввода
                 return true
             }
-
             override fun onQueryTextChange(newText: String?): Boolean {
-                val filteredCharacters = characters?.filter { it.name.startsWith(newText ?: "", true) } ?: emptyList()
-                val adapter = CharacterAdapter(filteredCharacters, object: CharacterAdapter.OnClickListener{
-                    override fun onRegisterItemClick(id: Int) {
-                        onItemClick(id)
-                    }
-
-                })
-                binding.recyclerView.adapter = adapter
+                input = newText
+                filterAndShowCharacters()
                 return true
-
             }
-
         })
 
-        lifecycleScope.launch {
-            characters = BreakingBadAPI.RETROFIT_SERVICE.getCharacter()
-            val adapter = CharacterAdapter(characters ?: emptyList(), object: CharacterAdapter.OnClickListener{
-                override fun onRegisterItemClick(id: Int) {
-                    onItemClick(id)
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                season = when (position) {
+                    0 -> ""
+                    1 -> "1"
+                    2 -> "2"
+                    3 -> "3"
+                    4 -> "4"
+                    5 -> "5"
+                    else -> throw IllegalStateException("Unknown Season")
                 }
-
-            })
-            binding.recyclerView.adapter = adapter
-
+                filterAndShowCharacters()
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
         }
     }
-    fun onItemClick(id:Int){
+
+    private fun refreshCharacters() {
+        lifecycleScope.launch {
+            characters = BreakingBadAPI.RETROFIT_SERVICE.getCharacter()
+            filterAndShowCharacters()
+        }
+    }
+
+    private fun filterAndShowCharacters() {
+        val filteredBySeasonCharacters = if (season == "")
+            characters
+        else
+            characters?.filter { it.season.contains(season) }
+        val filteredCharacters = filteredBySeasonCharacters?.filter { it.name.startsWith(input ?: "", true) }
+
+        val adapter = CharacterAdapter(filteredCharacters?: emptyList(), object: CharacterAdapter.OnClickListener{
+            override fun onRegisterItemClick(id: Int) {
+                onItemClick(id)
+            }
+        })
+        binding.recyclerView.adapter = adapter
+    }
+
+    private fun onItemClick(id:Int){
         val action = Screen1FragmentDirections.actionScreen1FragmentToScreen2Fragment(id)
         findNavController().navigate(action)
     }
